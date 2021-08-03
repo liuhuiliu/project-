dd8679887a690648e8159f8e26aad5591e2413b1
liuhuiliu <765226421@qq.com>
2021-08-03 11:38:22 +0800

    2021.8.3

---
+++ src/components/NotebookList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="notebook-list">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li><router-link to="/note/1">笔记本1</router-link></li>
+      <li><router-link to="/note/2">笔记本2</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "笔记本列表",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Notedetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="note-detail">
+    <h1>{{ msg }} : {{ $router.params.noteId }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "笔记详情页",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
