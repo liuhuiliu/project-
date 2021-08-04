681247fb8f6c39a9fa88e691b1e9b2695354ed92
liuhuiliu <765226421@qq.com>
2021-08-04 22:58:14 +0800

    21.8.4

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,48 @@
 <template>
-  <div>
-    <h1>LoginPage</h1>
-    <div>{{ text }}</div>
+  <div id="login">
+    <div class="model-mask">
+      <div class="model-wrapper">
+        <div class="model-container">
+          <div class="title">
+            <h2>简易云笔记</h2>
+          </div>
+          <div class="form">
+            <h3 @click="showRegister">注册账号</h3>
+            <div v-show="isShowRegister" class="register">
+              <input
+                type="text"
+                v-model="register.username"
+                placeholder="username"
+              />
+              <input
+                type="password"
+                v-model="register.password"
+                placeholder="password"
+              />
+              <p v-bind:class="{ error: register.isError }">
+                {{ register.notice }}
+              </p>
+              <div class="button">创建</div>
+            </div>
+            <h3 @click="showLogin">登陆账号</h3>
+            <div v-show="isShowLogin" class="login">
+              <input
+                type="text"
+                v-model="login.username"
+                placeholder="username"
+              />
+              <input
+                type="password"
+                v-model="login.password"
+                placeholder="password"
+              />
+              <p v-bind:class="{ error: login.isError }">{{ login.notice }}</p>
+              <div class="button">登陆</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,27 +51,122 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      text: "texttext",
+      isShowLogin: true,
+      isShowRegister: false,
+      login: {
+        username: "hunger",
+        password: "123456",
+        notice: "请输入用户名与密码",
+        isError: false,
+      },
+      register: {
+        username: "xxx",
+        password: "xxx",
+        notice: "创建账号后请记住用户名与密码",
+        isError: false,
+      },
     };
+  },
+  methods: {
+    showRegister() {
+      this.isShowLogin = true;
+      this.isShowRegister = false;
+    },
+    showLogin() {
+      this.isShowLogin = false;
+      this.isShowRegister = true;
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
+<style lang="less" scoped>
+.model-mask {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: table;
+  transition: opacity 0.3s ease;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.model-wrapper {
+  display: table-cell;
+  vertical-align: middle;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.model-container {
+  width: 270px;
+  height: auto;
+  margin: 0 auto;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+  // display: flex;
+  .title {
+    text-align: center;
+    padding: 10px 10px;
+  }
+  .form {
+    width: 270px;
+    border-left: 1px solid #ccc;
+    h3 {
+      padding: 10px 20px;
+      font-weight: normal;
+      font-size: 16px;
+      border-top: 1px solid #eee;
+      cursor: pointer;
+
+      &:nth-of-type(2) {
+        border-bottom: 1px solid #eee;
+      }
+    }
+    .button {
+      background-color: #2bb964;
+      height: 36px;
+      line-height: 36px;
+      text-align: center;
+      font-weight: bold;
+      color: #fff;
+      border-radius: 4px;
+      margin-top: 18px;
+      cursor: pointer;
+    }
+    .login,
+    .register {
+      padding: 10px 20px;
+      border-top: 1px solid #eee;
+      input {
+        display: block;
+        width: 100%;
+        height: 35px;
+        line-height: 35px;
+        padding: 0 6px;
+        border-radius: 4px;
+        border: 1px solid #ccc;
+        outline: none;
+        font-size: 14px;
+        margin-top: 10px;
+      }
+      input:focus {
+        border: 3px solid #9dcaf8;
+      }
+      p {
+        font-size: 12px;
+        margin-top: 10px;
+        color: #444;
+      }
+      .error {
+        color: red;
+      }
+    }
+    .login {
+      border-top: 0;
+    }
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,34 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '../components/Login.vue'
+import NoteBooks from '../components/NotebookList.vue'
+import Note from '../components/Notedetail.vue'
+import Trash from '../components/TrashDetail.vue'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     routes: [{
             path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld
+            redirect: '/notebooks',
+            component: NoteBooks
         },
         {
             path: '/login',
-            name: 'Login',
             component: Login
+        },
+        {
+            path: '/notebooks',
+            component: NoteBooks
+        },
+        {
+            path: '/note/:noteId',
+            component: Note
+        },
+        {
+            path: '/trash/:noteId',
+            component: Trash
         }
     ]
 })
+
+export default router;
